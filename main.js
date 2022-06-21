4d5efa50f8980c25e5b2c8e35d65a0c04f104b51
juan silva <juan_silva_o@hotmail.com>
2022-06-21 17:58:56 -0400

    Reto grupo 1

---
+++ main.js
@@ -0,0 +1,16 @@
+let nota1 = parseInt(prompt('Ingese la nota 1'));
+let nota2 = parseInt(prompt('Ingese la nota 2'));
+let nota3 = parseInt(prompt('Ingese la nota 3'));
+let nota4 = parseInt(prompt('Ingese la nota 4'));
+let nota5 = parseInt(prompt('Ingese la nota 5'));
+
+let prom = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
+let promRound = Math.round(prom)
+if(promRound >= 70){
+    document.write(`Su nota es: ${promRound} aprobó`);
+}
+else {
+    document.write(`Su nota es: ${promRound} reprobó`);
+
+}
+
